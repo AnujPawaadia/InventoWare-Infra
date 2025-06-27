aaed8fe4e3341286b16f2993457190ddff24a585
Anuj Pawadia <124757784+AnujPawaadia@users.noreply.github.com>
2025-06-27 18:35:16 +0530

    s

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,13 +10,6 @@ output "nlb_dns_name" {
   value       = aws_lb.nlb.dns_name
 }
 
-output "nlb_eips" {
-  description = "EIP addresses attached to the NLB"
-  value = [
-    for eip in aws_eip.nlb_eip : eip.public_ip
-  ]
-}
-
 output "monitoring_node_ip" {
   description = "Public IP of the monitoring node"
   value       = aws_instance.monitoring_node.public_ip
