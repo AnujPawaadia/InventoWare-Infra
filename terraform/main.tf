fd205c73fd21ee0632b42b7e28c6be92575ed652
Anuj Pawadia <124757784+AnujPawaadia@users.noreply.github.com>
2025-06-27 14:54:04 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -173,13 +173,3 @@ EOF
   )
 }
 
-# ======= OUTPUTS =======
-output "load_balancer_dns" {
-  description = "Public DNS of the Load Balancer"
-  value       = aws_lb.app_lb.dns_name
-}
-
-output "monitoring_public_ip" {
-  description = "Public IP of the monitoring instance"
-  value       = aws_instance.monitoring_node.public_ip
-}
