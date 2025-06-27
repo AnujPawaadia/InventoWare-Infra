e2089a40f67efdf68b5e23a8e4f6b1baeb7913d0
Anuj Pawadia <124757784+AnujPawaadia@users.noreply.github.com>
2025-06-27 18:36:35 +0530

    ew

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,9 +5,11 @@ output "monitoring_instance_public_ip" {
   value       = aws_instance.monitoring_node.public_ip
 }
 
-output "nlb_dns_name" {
-  description = "DNS name of the Network Load Balancer"
-  value       = aws_lb.nlb.dns_name
+output "nlb_eips" {
+  description = "EIP addresses attached to the NLB"
+  value = [
+    for eip in aws_eip.nlb_eip : eip.public_ip
+  ]
 }
 
 output "monitoring_node_ip" {
