a8f625bb50328160a1efc22aa2fcc286a6d6ec7e
Anuj Pawadia <124757784+AnujPawaadia@users.noreply.github.com>
2025-06-27 14:54:21 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,9 @@
-output "instance_public_ip" {
-  description = "Public IP of EC2 Instance"
-  value       = aws_instance.invento.public_ip
+output "load_balancer_dns" {
+  description = "Public DNS of the Load Balancer"
+  value       = aws_lb.app_lb.dns_name
+}
+
+output "monitoring_public_ip" {
+  description = "Public IP of the monitoring instance"
+  value       = aws_instance.monitoring_node.public_ip
 }
