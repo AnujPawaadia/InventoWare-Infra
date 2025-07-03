84ee9598c3af5ebc53f51733febd2157a42d2806
Anuj Pawadia <124757784+AnujPawaadia@users.noreply.github.com>
2025-07-03 09:11:26 +0530

    output.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,14 @@
-output "blue_instance_eip" {
-  description = "Elastic IP of the Blue instance"
+output "blue_instance_ip" {
+  description = "Public IP address of the Blue deployment instance"
   value       = aws_eip.blue_eip.public_ip
 }
 
-output "green_instance_eip" {
-  description = "Elastic IP of the Green instance"
+output "green_instance_ip" {
+  description = "Public IP address of the Green deployment instance"
   value       = aws_eip.green_eip.public_ip
 }
 
-output "monitor_instance_eip" {
-  description = "Elastic IP of the Monitor instance"
+output "monitoring_instance_ip" {
+  description = "Public IP address of the Monitoring instance"
   value       = aws_eip.monitor_eip.public_ip
 }
