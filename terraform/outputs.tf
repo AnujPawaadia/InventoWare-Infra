7d9343faa8f65d4895bf62d1885563b8b2d04318
Anuj Pawadia <124757784+AnujPawaadia@users.noreply.github.com>
2025-07-03 09:08:26 +0530

    updates

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,8 +12,3 @@ output "monitor_instance_eip" {
   description = "Elastic IP of the Monitor instance"
   value       = aws_eip.monitor_eip.public_ip
 }
-
-output "load_balancer_dns" {
-  description = "DNS name of the Application Load Balancer"
-  value       = aws_lb.app_lb.dns_name
-}
