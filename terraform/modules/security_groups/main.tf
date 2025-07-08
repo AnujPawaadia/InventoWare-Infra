f9157bdcfc4ae48b27148565cbbc87714d8c64dc
Arjun02BH <arjunsharma08meerut@gmail.com>
2025-07-09 01:14:27 +0530

    Update main.tf

--- terraform/modules/security_groups/main.tf
+++ terraform/modules/security_groups/main.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "bastion_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.bastion_allowed_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
